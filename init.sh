a42e14b4b2893dfae2ddaf11107ff817e3fbe9c9
Matthieu Baerts <matttbe@gmail.com>
2014-12-28 15:08:41 +0100

    init: remove encryption

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ docker pull kylemanna/openvpn
 docker run --name $OVPN_DATA -v /etc/openvpn busybox
 docker run --volumes-from $OVPN_DATA --rm kylemanna/openvpn ovpn_genconfig -u $PROT://$ADDR:$PORT
 docker run --volumes-from $OVPN_DATA --rm -it kylemanna/openvpn ovpn_initpki
-# docker run --volumes-from $OVPN_DATA --rm -i kylemanna/openvpn printf 'cipher none\n' >> /etc/openvpn/openvpn.conf
+docker run --volumes-from $OVPN_DATA --rm -i kylemanna/openvpn printf 'cipher none\n' >> /etc/openvpn/openvpn.conf
 
